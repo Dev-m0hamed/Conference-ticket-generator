d2ba1276820750844350a32f216e87d18b962897
Mohamed <010232066mo@gmail.com>
2025-11-09 10:45:57 +0200

    inputs checking

--- ticket/src/components/Input.jsx
+++ ticket/src/components/Input.jsx
@@ -4,21 +4,28 @@ function Input({ name, label, type = "text", placeholder = "" }) {
   const [value, setValue] = useState("");
   const [hasError, setHasError] = useState(false);
 
-  function handleChange(e) {
+  function validateInput(e) {
     setValue(e.target.value);
-    if (e.target.value.trim()) setHasError(false);
   }
 
   function handleBlur() {
-    if (!value.trim() && name === "fullName") setHasError(true);
+    if (name === "fullName") {
+      setHasError(value.trim() === "");
+    } else if (name === "email") {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      setHasError(!emailRegex.test(value));
+    } else if (name === "gitHub") {
+      setHasError(value.trim() === "");
+    } else setHasError(false);
   }
+
   return (
     <>
       <label htmlFor={name} className="block text-lg mt-4 mb-2 md:mt-3">
         {label}
       </label>
       <input
-        onChange={handleChange}
+        onChange={validateInput}
         onBlur={handleBlur}
         id={name}
         name={name}
@@ -30,6 +37,33 @@ function Input({ name, label, type = "text", placeholder = "" }) {
           hasError && "error-border"
         }`}
       />
+      {hasError && name === "email" && (
+        <div aria-live="assertive" className="items-center text-xs mt-2 error">
+          <svg
+            className="mr-1"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="none"
+            viewBox="0 0 16 16"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z"
+            />
+            <path fill="currentColor" d="M8.004 10.462V7.596ZM8 5.57v-.042Z" />
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.004 10.462V7.596M8 5.569v-.042"
+            />
+          </svg>
+          <span>Please enter a valid email address.</span>
+        </div>
+      )}
     </>
   );
 }
--- ticket/src/pages/FormPage.jsx
+++ ticket/src/pages/FormPage.jsx
@@ -29,39 +29,6 @@ function FormPage() {
             placeholder={"example@email.com"}
           />
 
-          {/* <div
-            id="emailError"
-            aria-live="assertive"
-            className="hidden items-center text-xs mt-2"
-          >
-            <svg
-              className="mr-1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="none"
-              viewBox="0 0 16 16"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z"
-              />
-              <path
-                fill="currentColor"
-                d="M8.004 10.462V7.596ZM8 5.57v-.042Z"
-              />
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.004 10.462V7.596M8 5.569v-.042"
-              />
-            </svg>
-            <span>Please enter a valid email address.</span>
-          </div> */}
-
           <Input
             name={"gitHub"}
             label={"GitHub Username"}
